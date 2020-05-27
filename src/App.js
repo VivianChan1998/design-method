432d1689c9932e0893ba621e55119650655102c2
Vivian <b05901179@ntu.edu.tw>
2020-05-27 21:04:51 +0800

    init gh-pages and test

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>:)</h1>
+      <Temp />
+    </div>
+  );
+}
+
+function Temp(){
+  return(
+    <h1>e223123</h1>
+  )
+}
+
+export default App;
