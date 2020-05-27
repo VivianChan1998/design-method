710e35e60144dd5591fc51c9c377eef6c5037b71
Vivian <b05901179@ntu.edu.tw>
2020-05-27 20:48:24 +0800

    Updates

---
+++ precache-manifest.e1574e9daf585f3dcee5dfbdf2166cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e15d347ba5c7a2440fd6739209e09fe",
+    "url": "/design-method-team2/index.html"
+  },
+  {
+    "revision": "2dffb4613b9e13b6a4f0",
+    "url": "/design-method-team2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2095c1e4a37a808c4d56",
+    "url": "/design-method-team2/static/js/2.4f79c7c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/design-method-team2/static/js/2.4f79c7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dffb4613b9e13b6a4f0",
+    "url": "/design-method-team2/static/js/main.71e04ef6.chunk.js"
+  },
+  {
+    "revision": "a82b7faea690062ecd93",
+    "url": "/design-method-team2/static/js/runtime-main.5788b75d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/design-method-team2/static/media/logo.5d5d9eef.svg"
+  }
+]);
